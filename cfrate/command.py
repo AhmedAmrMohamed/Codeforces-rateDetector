f4bde90cbe286f7c9a08789ce9e44ef6e8433284
theunderd0g <ahmedbonumstelio@gmail.com>
2018-11-01 15:40:43 +0200

    a bug fixed in command.py

--- cfrate/command.py
+++ cfrate/command.py
@@ -2,10 +2,9 @@ from cfrate import *
 import os
 def main():
     platform =  os.sys.platform.lower()
-    if 'win' in platform:
+    if  'win' in platform:
         from cfrate.filehandles import main
         main()
     else:
-        from cfrate.browswerhandles import main
+        from cfrate.browserhandles import main
         main()
-
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cfrate",
-    version="3.0.0",
+    version="3.0.3",
     author="theunderdog",
     author_email="ahmedbonumstelio@gmail.com",
     description="play a file sound - song- once the rating changes at all the participants  of some round. Also if a handle was given the program will output the the changes to that handle",
